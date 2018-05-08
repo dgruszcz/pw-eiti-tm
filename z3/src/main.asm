4ccd4a0ea275c9b86753c5f2bb4f5bd0da127a9e
Dawid Gruszczyński <33604720+dgruszcz@users.noreply.github.com>
2018-05-08 15:07:27 +0200

    Update main.asm

--- z3/src/main.asm
+++ z3/src/main.asm
@@ -16,33 +16,59 @@
                                             ; references to current section.
 
 ;-------------------------------------------------------------------------------
+
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 			mov.b	#0xff, P3DIR			; Set P3 as output
 			mov.b	#0x01, P1IES
 			mov.b	#0x01, P1IE
-			mov.b	#0x01, P2IES
-			mov.b	#0x01, P2IE
-			mov.w   #(0x08),SR
+			mov.b	#0x02, P2IES
+			mov.b	#0x02, P2IE
 
-			mov.b	#0x01, P3OUT
-			;mov.w   #isr, &0xffe2
+			mov.w	#0x00, R5 				; Debouncing register
+
+			mov.b	#0x00, P3OUT
 
 ;-------------------------------------------------------------------------------
 ; Main loop here
 ;-------------------------------------------------------------------------------
 loop:
+	 		bis 	#GIE+CPUOFF+SCG1+SCG0, SR
+loop_1:
+
+ 			bit.b	#0x02, P2IN
+			jnz 	loop_2
+
+			inc 	R5
+			cmp		#0xFFF, R5
+			jnz		loop_1
+			inc 	P3OUT
+loop_2:
+			clr		R5
+			mov.b	#0x00, P2IFG
+			dint
+			mov.b	#0x02, P2IE
 			jmp		loop
 
-
-isrP1:
-			mov.b	#0xFF, P3OUT
-			mov.b	#0x00, P1IFG
-			RETI
-isrP2:
+;-------------------------------------------------------------------------------
+; ISRs
+;-------------------------------------------------------------------------------
+isrP1:  ; reset
 			mov.b	#0x00, P3OUT
+			bit.b	#0x01, P1IN
+
+			jz 	isrP1_1
+			mov.b	#0x00, P1IFG
 			mov.b	#0x00, P2IFG
-			RETI
+isrP1_1:
+			mov.w   #loop_2, 2(SP)
+			reti
+isrP2:  ; licz
+			mov.b	#0x00, P2IFG
+			bic		#CPUOFF+SCG1+SCG0, 0(SP)
+			mov.b	#0x00, P2IE
+			reti
+
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
 ;-------------------------------------------------------------------------------
@@ -53,13 +79,8 @@ isrP2:
 ; Interrupt Vectors
 ;-------------------------------------------------------------------------------
             .sect   ".reset"                ; MSP430 RESET Vector
-            .short  RESET                   ; dalsze to tabelka od dolu!!!
+            .short  RESET
             .sect	".int04"
             .short  isrP1
             .sect	".int01"
             .short  isrP2
-
-
-
-
-
