c66849f8e4bdd64169f92ab21ecfa435e8cf8213
Dawid Gruszczyński <33604720+dgruszcz@users.noreply.github.com>
2018-05-15 18:26:05 +0200

    dodane stałe

--- z3/src/main.asm
+++ z3/src/main.asm
@@ -19,13 +19,19 @@
 
 RESET       mov.w   #__STACK_END,SP         	; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL 	; Stop watchdog timer
+
+;-------------------------------------------------------------------------------
+Init	
+			CLK_BUT .set 0x08	; Przycisk CLK
+			CLR_BUT .set 0x01 	; Przycisk CLR
+			
 			mov.b	#0xff, P3DIR	; Set P3 as output
 
-			mov.b	#0x01, P1IES
-			mov.b	#0x01, P1IE
+			mov.b	#CLR_BUT, P1IES ; Konfiguracja przerwań CLR
+			mov.b	#CLR_BUT, P1IE
 
-			mov.b	#0x08, P2IES
-			mov.b	#0x08, P2IE
+			mov.b	#CLK_BUT, P2IES	; Konfiguracja przerwań CLK
+			mov.b	#CLK_BUT, P2IE
 
 			mov.w	#0x00, R5 	; Debouncing register
 			mov.w	#0x00, R6 	; Rejestr pomocniczy
@@ -39,11 +45,11 @@ loop:
 	 		bis 	#GIE+CPUOFF+SCG1+SCG0, SR
 loop_1:
 			eint
-			bit.b	#0x01, P1IN
+			bit.b	#CLR_BUT, P1IN
 			jz	loop_1
 			bit.b	#0x01, R6
 			jnz	loop_3
- 			bit.b	#0x08, P2IN
+ 			bit.b	#CLK_BUT, P2IN
 			jnz 	loop_3
 			inc 	R5
 			dint
@@ -61,14 +67,14 @@ loop_3:
 			mov.b	#0x00, P2IFG
 			dint
 			clr     R6
-			mov.b	#0x08, P2IE
+			mov.b	#CLK_BUT, P2IE
 			jmp	loop
 
 
 ;-------------------------------------------------------------------------------
 ; ISRs
 ;-------------------------------------------------------------------------------
-isrP1:  ; Reset
+isrP1:  ; Przerwania przycisku CLR
 			mov.b	#0x00, P3OUT
 			mov.b	#0x00, P1IFG
 			mov.b	#0x00, P2IFG
@@ -78,7 +84,7 @@ isrP1:  ; Reset
 			bic	#CPUOFF+SCG1+SCG0, 0(SP)
 			reti
 
-isrP2:  ; Licz
+isrP2:  ; Przerwania przycisku CLK
 			bic	#CPUOFF+SCG1+SCG0, 0(SP)
 			mov.b	#0x00, P2IE
 			reti
