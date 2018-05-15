62f4ceec2fa008467fe4fb3521f290aec3eacec6
Dawid Gruszczyński <33604720+dgruszcz@users.noreply.github.com>
2018-05-15 18:50:18 +0200

    Update main.asm

--- z3/src/main.asm
+++ z3/src/main.asm
@@ -12,29 +12,29 @@
             .text                           ; Assemble into program memory.
             .retain                         ; Override ELF conditional linking
                                             ; and retain current section.
-            .retainrefs                     ; And retain any sections that have
+           	.retainrefs                     ; And retain any sections that have
                                             ; references to current section.
 
 ;-------------------------------------------------------------------------------
 
-RESET       mov.w   #__STACK_END,SP         	; Initialize stackpointer
+RESET		mov.w   #__STACK_END,SP         ; Initialize stackpointer
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL 	; Stop watchdog timer
 
 ;-------------------------------------------------------------------------------
 Init	
-			CLK_BUT .set 0x08	; Przycisk CLK
-			CLR_BUT .set 0x01 	; Przycisk CLR
+			CLK_BUT .set 0x08		; Przycisk CLK
+	    	CLR_BUT .set 0x01 		; Przycisk CLR
 			
-			mov.b	#0xff, P3DIR	; Set P3 as output
+	    	mov.b	#0xff, P3DIR	; Set P3 as output
 
-			mov.b	#CLR_BUT, P1IES ; Konfiguracja przerwań CLR
+	    	bis.b	#CLR_BUT, P1IES ; Konfiguracja przerwań CLR
 			mov.b	#CLR_BUT, P1IE
 
-			mov.b	#CLK_BUT, P2IES	; Konfiguracja przerwań CLK
+			bis.b	#CLK_BUT, P2IES	; Konfiguracja przerwań CLK
 			mov.b	#CLK_BUT, P2IE
 
-			mov.w	#0x00, R5 	; Debouncing register
-			mov.w	#0x00, R6 	; Rejestr pomocniczy
+			mov.w	#0x00, R5 		; Debouncing register
+			mov.w	#0x00, R6 		; Rejestr pomocniczy
 	
 			mov.b	#0x00, P3OUT
 
@@ -46,29 +46,29 @@ loop:
 loop_1:
 			eint
 			bit.b	#CLR_BUT, P1IN
-			jz	loop_1
+			jz		loop_1
 			bit.b	#0x01, R6
-			jnz	loop_3
+			jnz		loop_3
  			bit.b	#CLK_BUT, P2IN
 			jnz 	loop_3
 			inc 	R5
 			dint
-			cmp	#0x04FF, R5
-			jnz	loop_1
-			cmp	P4IN, P3OUT
-			jl	loop_2
+			cmp		#0x04FF, R5
+			jnz		loop_1
+			cmp		P4IN, P3OUT
+			jl		loop_2
 			mov.b	#0x00, P3OUT
-			jmp	loop_3
+			jmp		loop_3
 loop_2:
 			inc 	P3OUT
 loop_3:
 			eint
-			clr	R5
-			mov.b	#0x00, P2IFG
+			clr		R5
+			bic.b	#CLK_BUT, P2IFG
 			dint
 			clr     R6
-			mov.b	#CLK_BUT, P2IE
-			jmp	loop
+			bis.b	#CLK_BUT, P2IE
+			jmp		loop
 
 
 ;-------------------------------------------------------------------------------
@@ -76,17 +76,17 @@ loop_3:
 ;-------------------------------------------------------------------------------
 isrP1:  ; Przerwania przycisku CLR
 			mov.b	#0x00, P3OUT
-			mov.b	#0x00, P1IFG
-			mov.b	#0x00, P2IFG
-			mov.b	#0x00, P2IE
+			bic.b	#CLR_BUT, P1IFG
+			bic.b	#CLK_BUT, P2IFG
+			bic.b	#CLK_BUT, P2IE
 			clr     R5
 			bis.w   #0x01, R6
-			bic	#CPUOFF+SCG1+SCG0, 0(SP)
+			bic		#CPUOFF+SCG1+SCG0, 0(SP)
 			reti
 
 isrP2:  ; Przerwania przycisku CLK
-			bic	#CPUOFF+SCG1+SCG0, 0(SP)
-			mov.b	#0x00, P2IE
+			bic		#CPUOFF+SCG1+SCG0, 0(SP)
+			bic.b	#CLK_BUT, P2IE
 			reti
 
 ;-------------------------------------------------------------------------------
