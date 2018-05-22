d68b7d0c7fc983743f0cb4c5808db6f3f1553b69
Krystian Chachuła <krystiancha@gmail.com>
2018-05-23 00:08:18 +0200

    Minor layout fixes

--- z3/src/main.asm
+++ z3/src/main.asm
@@ -5,24 +5,23 @@
             .retainrefs
 RESET       mov.w   #__STACK_END,SP
 StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL
-
 ;-------------------------------------------------------------------------------
 ; Init
 ;-------------------------------------------------------------------------------
-CLK_BUT .set 0x08       ; Przycisk CLK
-CLR_BUT .set 0x01       ; Przycisk CLR
-DEB_TIM .set 0x00ff		; czas debouncingu
+CLK_BUT .set 0x08                       ; przycisk CLK
+CLR_BUT .set 0x01                       ; przycisk CLR
+DEB_TIM .set 0x00ff                     ; czas debouncingu
 
-            mov.b   #0xff, &P3DIR    ; Set P3 as output
+            mov.b   #0xff, &P3DIR       ; ustaw P3 jako wyjscie
 
-            bis.b   #CLR_BUT, &P1IES ; Konfiguracja przerwan CLR
+            bis.b   #CLR_BUT, &P1IES    ; konf. przerwan CLR
             mov.b   #CLR_BUT, &P1IE
 
-            bis.b   #CLK_BUT, &P2IES ; Konfiguracja przerwan CLK
+            bis.b   #CLK_BUT, &P2IES    ; konf. przerwan CLK
             mov.b   #CLK_BUT, &P2IE
 
-            mov.w   #0x00, R5       ; Debouncing register
-            mov.w   #0x00, R6       ; Rejestr pomocniczy
+            mov.w   #0x00, R5           ; rejestr do debouncingu
+            mov.w   #0x00, R6           ; rejestr pomocniczy
 
             mov.b   #0x00, &P3OUT
 ;-------------------------------------------------------------------------------
@@ -42,35 +41,35 @@ loop_1:
             jnz     no_inc
             inc     R5
 no_inc:
-			inc		R7
+            inc     R7
             cmp     #DEB_TIM, R7
             jnz     loop_1
             dint
-            cmp 	#0, R5
-            jz		loop_3
+            cmp     #0, R5
+            jz      loop_3
             cmp     R5, R7
-            jz		main_inc
-			eint
-			call	#clear
-			jmp		loop_1
+            jz      main_inc
+            eint
+            call    #clear
+            jmp     loop_1
 
 main_inc:
-			bit.w	#0x02, R6
-			call	#clear
-        	jnz		loop_1
-            bis.w	#0x02, R6
+            bit.w   #0x02, R6
+            call    #clear
+            jnz     loop_1
+            bis.w   #0x02, R6
             cmp     &P4IN, &P3OUT
             jl      loop_2
             mov.b   #0x00, &P3OUT
-           	call	#clear
+            call    #clear
             jmp     loop_1
 loop_2:
             inc     &P3OUT
-            call	#clear
-            jmp		loop_1
+            call    #clear
+            jmp     loop_1
 
 loop_3:
-            call	#clear
+            call    #clear
             bic.b   #CLK_BUT, &P2IFG
             clr     R6
             bis.b   #CLK_BUT, &P2IE
@@ -78,12 +77,12 @@ loop_3:
 
 clear:
             clr     R5
-            clr 	R7
+            clr     R7
             ret
 ;-------------------------------------------------------------------------------
 ; ISRs
 ;-------------------------------------------------------------------------------
-isrP1:      ; Przerwania przycisku CLR
+isrP1:      ; przerwania przycisku CLR
             mov.b   #0x00, &P3OUT
             bic.b   #CLR_BUT, &P1IFG
             bic.b   #CLK_BUT, &P2IFG
@@ -93,7 +92,7 @@ isrP1:      ; Przerwania przycisku CLR
             bic     #CPUOFF+SCG1+SCG0, 0(SP)
             reti
 
-isrP2:      ; Przerwania przycisku CLK
+isrP2:      ; przerwania przycisku CLK
             bic     #CPUOFF+SCG1+SCG0, 0(SP)
             bic.b   #CLK_BUT, &P2IE
             reti
@@ -105,10 +104,9 @@ isrP2:      ; Przerwania przycisku CLK
 ;-------------------------------------------------------------------------------
 ; Interrupt Vectors
 ;-------------------------------------------------------------------------------
-            .sect   ".reset"                ; MSP430 RESET Vector
+            .sect   ".reset"    ; MSP430 RESET Vector
             .short  RESET
             .sect   ".int04"
             .short  isrP1
             .sect   ".int01"
             .short  isrP2
-
