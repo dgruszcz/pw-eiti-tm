aaf090aacfaf3a1b5dd08407bd8940b4a887fce0
Dawid Gruszczyński <33604720+dgruszcz@users.noreply.github.com>
2018-05-25 18:27:48 +0200

    Update CtdnTimer.h

--- z4/PUSH AF FTW/CtdnTimer.h
+++ z4/PUSH AF FTW/CtdnTimer.h
@@ -1,3 +1,6 @@
+#ifndef CTDN_TIMER_H_
+#define CTDN_TIMER_H_
+
 #include <stdint.h>
 #include <stdlib.h>
 
@@ -8,3 +11,5 @@ typedef struct CtdnTimer {
 CtdnTimer *ctdnTimerInit();
 uint8_t ctdnTimerDecrement(CtdnTimer *ctdnTimer);
 void ctdnTimerSetSeconds(CtdnTimer *ctdnTimer, int16_t seconds);
+
+#endif /* CTDN_TIMER_H_ */
