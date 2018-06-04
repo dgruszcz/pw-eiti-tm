0a875c7a2dac1e67ee69d783a8010deab36a6fc1
Jack-the-bubble <ms1b.139@gmail.com>
2018-06-04 16:11:55 +0200

    Update CircularBuffer.c

--- z5/src/CircularBuffer.c
+++ z5/src/CircularBuffer.c
@@ -1,8 +1,8 @@
 #include "CircularBuffer.h"
 
-CircularBuffer *circularBufferInit(size_t size) {
+CircularBuffer *circularBufferInit(size_t size, char *bufBuf) {
 	CircularBuffer *circularBuffer = malloc((size_t) sizeof(CircularBuffer));
-	circularBuffer->buffer = malloc(size * sizeof(char));
+	circularBuffer->buffer = bufBuf;
 
 	circularBuffer->size = size;
 	circularBuffer->read = circularBuffer->buffer;
@@ -75,32 +75,32 @@ size_t freeSpace1(CircularBuffer *self) {
 }
 
 size_t circularBufferRead1(CircularBuffer *self, char *data, size_t length) {
-	size_t read = 0;
+	__bic_SR_register(GIE);
 	int dataLength = self->writePointer - self->readPointer; // Ilosc bajtow zapisanych w buforze
 
-	dataLength = length > dataLength ? dataLength : length;  // Ilosc bajtow do oczytania
-	read = dataLength;
-	while (dataLength--){
-    *(data++) = self->buffer[self->readPointer++ % self->size];
+	if (dataLength > 0) {
+		*data = self->buffer[self->readPointer++ % self->size];
+		if (self->readPointer >= self->size && self->writePointer >= self->size){
+			self->readPointer = self->readPointer % self->size;
+			self->writePointer = self->writePointer % self->size;
+		}
+		__bis_SR_register(GIE);
+		return 1;
 	}
-
-	if (self->readPointer >= self->size && self->writePointer >= self->size){
-    self->readPointer = self->readPointer % self->size;
-    self->writePointer = self->writePointer % self->size;
-	}
-	return read;
+	__bis_SR_register(GIE);
+	return 0;
 }
 
 size_t circularBufferWrite1(CircularBuffer *self,char *data, size_t length) {
-	size_t written = 0;
+	__bic_SR_register(GIE);
 	int dataLength = self->size - (self->writePointer - self->readPointer); // Dostepne miejsce w buforze
-
-	dataLength = length > dataLength ? dataLength : length;  // Ilosc bajtow mozliwych do zapisania
-	written = dataLength;
-	while (dataLength--){
-    self->buffer[self->writePointer++ % self->size] = *(data++);
+	if (dataLength > 0) {
+		self->buffer[self->writePointer++ % self->size] = *data;
+		__bis_SR_register(GIE);
+		return 1;
 	}
-	return written;
+	__bis_SR_register(GIE);
+    return 0;
 }
 
 
