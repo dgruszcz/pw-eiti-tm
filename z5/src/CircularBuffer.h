522b2095c4dc795b8f1d2aca19d7080b60740feb
Jack-the-bubble <ms1b.139@gmail.com>
2018-06-04 16:12:23 +0200

    Update CircularBuffer.h

--- z5/src/CircularBuffer.h
+++ z5/src/CircularBuffer.h
@@ -1,6 +1,7 @@
 #ifndef CIRCULARBUFFER_H_
 #define CIRCULARBUFFER_H_
 
+#include <msp430.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -14,7 +15,7 @@ typedef struct CircularBuffer {
 	uint8_t writePointer;
 } CircularBuffer;
 
-CircularBuffer *circularBufferInit(size_t size);
+CircularBuffer *circularBufferInit(size_t size, char *bufBuf);
 size_t circularBufferRead(CircularBuffer *self, char *data, size_t length);
 size_t circularBufferWrite(CircularBuffer *self, char *data, size_t length);
 size_t freeSpace(CircularBuffer *self);
